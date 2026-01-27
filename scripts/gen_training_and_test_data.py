44b601fa139914299851d466331c7dcbb1f7f3d9
ahmedtarek- <ahmed.t.abdelfattah@gmail.com>
2026-01-27 10:58:32 +0100

    [scripts][notebooks] updates code to handle passing of IDs to Dataset and Dataloader so that data can be reconstructed in order

--- scripts/gen_training_and_test_data.py
+++ scripts/gen_training_and_test_data.py
@@ -24,30 +24,30 @@ TEST_DATASET_PERCENTAGE = 10
 foldername = '/Users/tarek/Documents/UNI/Lab Rotations/Kremkow/Data/Stimuli-Responses-Delay'
 stimuli_file_names = [
     # '2022-12-20_15-08_1_stimulus_cb_200.npy',
-    '2022-12-20_15-08_2_stimulus_sn_dark.npy',
-    '2022-12-20_15-08_3_stimulus_sn_light.npy',
+    # '2022-12-20_15-08_2_stimulus_sn_dark.npy',
+    # '2022-12-20_15-08_3_stimulus_sn_light.npy',
 
     # '2023-03-15_11-05_1_stimulus_cb_200.npy',
     # '2023-03-15_11-05_2_stimulus_sn_dark.npy',
     # '2023-03-15_11-05_3_stimulus_sn_light.npy',
 
     # '2023-03-15_15-23_1_stimulus_cb_200.npy',
-    # '2023-03-15_15-23_2_stimulus_sn_dark.npy',
-    # '2023-03-15_15-23_3_stimulus_sn_light.npy'
+    '2023-03-15_15-23_2_stimulus_sn_dark.npy',
+    '2023-03-15_15-23_3_stimulus_sn_light.npy'
 ]
 
 fr_file_names = [
     # '2022-12-20_15-08_1_fr_cb_200.npy',
-    '2022-12-20_15-08_2_fr_sn_dark.npy',
-    '2022-12-20_15-08_3_fr_sn_light.npy',
+    # '2022-12-20_15-08_2_fr_sn_dark.npy',
+    # '2022-12-20_15-08_3_fr_sn_light.npy',
 
     # '2023-03-15_11-05_1_fr_cb_200.npy',
     # '2023-03-15_11-05_2_fr_sn_dark.npy',
     # '2023-03-15_11-05_3_fr_sn_light.npy',
 
     # '2023-03-15_15-23_1_fr_cb_200.npy',
-    # '2023-03-15_15-23_2_fr_sn_dark.npy',
-    # '2023-03-15_15-23_3_fr_sn_light.npy'
+    '2023-03-15_15-23_2_fr_sn_dark.npy',
+    '2023-03-15_15-23_3_fr_sn_light.npy'
 ]
 
 images_dim = np.load(os.path.join(foldername, stimuli_file_names[0])).shape
@@ -90,11 +90,12 @@ images = images.reshape(data_length, 1, images_dim[1], images_dim[2])
 index_to_split = data_length - int(data_length/TEST_DATASET_PERCENTAGE)
 images, test_images = np.split(images, [index_to_split])
 responses, test_responses = np.split(responses, [index_to_split])
+reconstruct_data, test_reconstruct_data = np.split(reconstruct_data, [index_to_split])
 
 
 # 6. Produce images_training, responses_training, images_test, responses_test
 save_folder = '/Users/tarek/Documents/UNI/Lab Rotations/Kremkow/Data/Training-SparseNoise'
-save_date = '2022-12-20_15-08'
+save_date = '2023-03-15_15-23'
 
 training_images_file_path = os.path.join(save_folder, "training_images_{}.npy".format(save_date))
 training_responses_file_path = os.path.join(save_folder, "training_responses_{}.npy".format(save_date))
@@ -102,7 +103,8 @@ training_responses_file_path = os.path.join(save_folder, "training_responses_{}.
 test_images_file_path = os.path.join(save_folder, "test_images_{}.npy".format(save_date))
 test_responses_file_path = os.path.join(save_folder, "test_responses_{}.npy".format(save_date))
 
-reconstruct_file_path = os.path.join(save_folder, "reconstruct_{}.npy".format(save_date))
+training_reconstruct_file_path = os.path.join(save_folder, "training_reconstruct_{}.npy".format(save_date))
+test_reconstruct_file_path = os.path.join(save_folder, "test_reconstruct_{}.npy".format(save_date))
 
 with open(training_images_file_path, 'wb') as f:
     np.save(f, images)
@@ -114,12 +116,15 @@ with open(test_images_file_path, 'wb') as f:
 with open(test_responses_file_path, 'wb') as f:
     np.save(f, test_responses)
 
-with open(reconstruct_file_path, 'wb') as f:
-    np.save(f, {'m': reconstruct_meta_data, 'd': reconstruct_data}, allow_pickle=True)
+with open(training_reconstruct_file_path, 'wb') as f:
+    np.save(f, {'meta': reconstruct_meta_data, 'ids': reconstruct_data}, allow_pickle=True)
+with open(test_reconstruct_file_path, 'wb') as f:
+    np.save(f, {'meta': reconstruct_meta_data, 'ids': test_reconstruct_data}, allow_pickle=True)
 
 print()
 print("Created ", training_images_file_path)
 print("Created ", training_responses_file_path)
 print("Created ", test_images_file_path)
 print("Created ", test_responses_file_path)
-print("Created ", reconstruct_file_path)
+print("Created ", training_reconstruct_file_path)
+print("Created ", test_reconstruct_file_path)
